73bb2203e7ebd628f50989c63e0bfe527f947712
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-07-30 20:14:45 +0800

    refactor: abstract as function

--- src/logger.py
+++ src/logger.py
@@ -1,20 +1,35 @@
 import os
+import sys
 import opendal
 
-# 设置阿里云OSS凭证
-os.environ['AWS_ACCESS_KEY_ID'] = ''
-os.environ['AWS_SECRET_ACCESS_KEY'] = ''
 
-# 初始化 S3 操作符
-op = opendal.Operator(
-    scheme="s3",
-    root="/",
-    bucket="laoshubaby",
-    region="cn-beijing",
-    endpoint="https://laoshubaby.oss-cn-beijing.aliyuncs.com",
-    enable_virtual_host_style="True",
-)
+def init_environ():
+    """
+    假定用户已经配置过aliyun-python-sdk
+    """
+    ALIYUN_ACCESSKEY_ID = os.environ.get("ALIYUN_ACCESSKEY_ID", "")
+    ALIYUN_ACCESSKEY_SECRET = os.environ.get("ALIYUN_ACCESSKEY_SECRET", "")
+    os.environ["AWS_ACCESS_KEY_ID"] = ALIYUN_ACCESSKEY_ID
+    os.environ["AWS_SECRET_ACCESS_KEY"] = ALIYUN_ACCESSKEY_SECRET
 
-# 读取文件内容
-file_content = op.read("/static/nihongo/nhgbkysms.metadata.json1")
-print(file_content.decode('utf-8'))
+
+def init_operator() -> opendal.Operator:
+    return opendal.Operator(
+        scheme="s3",
+        root="/",
+        bucket="laoshubaby",
+        region="cn-beijing",
+        endpoint="https://laoshubaby.oss-cn-beijing.aliyuncs.com",
+        enable_virtual_host_style="True",
+    )
+
+
+def get_file(op: opendal.Operator, path: str) -> str:
+    content = op.read(path)
+    return content.decode("utf-8")
+
+
+init_environ()
+op = init_operator()
+file_content = get_file(op, "/static/nihongo/nhgbkysms.metadata.json")
+print(file_content)
