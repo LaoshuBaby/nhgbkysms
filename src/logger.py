1b8c2be9fd3ddeba5ea49c67be89624910453cdd
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-07-30 20:45:18 +0800

    feat: finish I and O

--- src/logger.py
+++ src/logger.py
@@ -1,25 +1,23 @@
 import os
-import sys
+from datetime import datetime 
 import opendal
 
+S3_BUCKET="laoshubaby"
+S3_REGION="cn-beijing"
+S3_ENDPOINT="https://laoshubaby.oss-cn-beijing.aliyuncs.com"
 
-def init_environ():
+def init_operator() -> opendal.Operator:
     """
     假定用户已经配置过aliyun-python-sdk
     """
-    ALIYUN_ACCESSKEY_ID = os.environ.get("ALIYUN_ACCESSKEY_ID", "")
-    ALIYUN_ACCESSKEY_SECRET = os.environ.get("ALIYUN_ACCESSKEY_SECRET", "")
-    os.environ["AWS_ACCESS_KEY_ID"] = ALIYUN_ACCESSKEY_ID
-    os.environ["AWS_SECRET_ACCESS_KEY"] = ALIYUN_ACCESSKEY_SECRET
-
-
-def init_operator() -> opendal.Operator:
     return opendal.Operator(
         scheme="s3",
         root="/",
-        bucket="laoshubaby",
-        region="cn-beijing",
-        endpoint="https://laoshubaby.oss-cn-beijing.aliyuncs.com",
+        bucket=S3_BUCKET,
+        region=S3_REGION,
+        endpoint=S3_ENDPOINT,
+        access_key_id=os.environ.get("ALIYUN_ACCESSKEY_ID", ""),
+        secret_access_key=os.environ.get("ALIYUN_ACCESSKEY_SECRET", ""),
         enable_virtual_host_style="True",
     )
 
@@ -28,8 +26,10 @@ def get_file(op: opendal.Operator, path: str) -> str:
     content = op.read(path)
     return content.decode("utf-8")
 
+def set_file(op: opendal.Operator, path: str, content:str):
+    op.write(path, content.encode('utf-8'))
 
-init_environ()
 op = init_operator()
+set_file(op,"/static/nihongo/nhgbkysms.metadata.json1",str(datetime.now()))
 file_content = get_file(op, "/static/nihongo/nhgbkysms.metadata.json")
 print(file_content)
